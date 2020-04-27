8f07763a68cd57980fbdc2d18b7f27b0233e0d9d
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-27 15:16:05 +0700

    first commit

---
+++ cat/cat.dart
@@ -0,0 +1,6 @@
+class Cat {
+  String color;
+  double height;
+  double length;
+  double weight;
+}
---
+++ cat/cat.kt
@@ -0,0 +1,6 @@
+data class Cat {
+    var color: String = "",
+    var height: Double = 0.0,
+    var length: Double = 0.0,
+    var weight: Double = 0.0
+}
